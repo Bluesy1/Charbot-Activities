2841917fe6bb055ae7ae6418692448b927eb6008
Bluesy1 <68259537+Bluesy1@users.noreply.github.com>
2025-05-11 17:25:47 -0700

    Better-ish board

--- client/src/Sudoku.tsx
+++ client/src/Sudoku.tsx
@@ -1,5 +1,10 @@
 import { getSudoku } from 'sudoku-gen';
 import { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Stack from 'react-bootstrap/Stack';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import "./Sudoku.css";
 type CorrectCellContent = "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9";
 type CellContent = CorrectCellContent | "";
@@ -10,8 +15,7 @@ function isValidCellContent(content: string): content is CellContent {
   return content.length === 0 || (content.length === 1 && validChars.includes(content));
 }
 
-
-function Cell(initial: CellContent, key: number) {
+function Cell(initial: CellContent) {
   const [content, setContent] = useState(initial);
   const onChange = (value: string) => {
     if (isValidCellContent(value)) {
@@ -19,15 +23,17 @@ function Cell(initial: CellContent, key: number) {
     }
   }
   const elem = (
-    <td key={key}>
-      <input type="text" maxLength={1} value={content} disabled={initial !== ""}
+    <div>
+      <Form.Control maxLength={1} value={content} readOnly={initial !== ""}
         onChange={e => (onChange(e.target.value))}
         style={{
-          maxWidth: "10px", fontWeight: (initial !== "") ? "bold" : "normal",
-          opacity: 1, backgroundColor: "rgb(59, 59, 59)", color: "#fff"
+          width: "2em", height: "2em", padding: "8px",
+          fontWeight: (initial !== "") ? "bold" : "normal",
+          textAlign: "center"
+          // opacity: 1, backgroundColor: "rgb(59, 59, 59)", color: "#fff"
         }}
       />
-    </td>
+    </div>
   )
   return elem
 }
@@ -35,30 +41,78 @@ function Cell(initial: CellContent, key: number) {
 function Sudoku() {
   const sudoku = getSudoku("easy");
   const puzzle = Array.from(sudoku.puzzle) as Array<CellContent | "-">;
-  const solution = Array.from(sudoku.solution) as Array<CorrectCellContent>;
 
-  const [cells] = useState(puzzle.map((value, index) => { return { content: (value == "-") ? "" : value, correct: solution[index], key: index } }));
+  const [solution] = useState(Array.from(sudoku.solution) as Array<CorrectCellContent>);
+  const [cells] = useState(puzzle.map((value) => Cell((value == "-") ? "" : value)));
 
-  const board_ = new Array<Array<{ content: CellContent; correct: CorrectCellContent; key: number; }>>();
-
-  for (let index = 0; index < 9; index++) {
-    const row_start = index * 9;
-    board_.push(cells.slice(row_start, row_start + 9));
-  }
-
-  const [board] = useState(board_);
+  const GAP = 2;
 
   return (
-    <div>
-      <table>
-        <tbody>
-          {board.map((row, idx) =>
-            <tr key={idx}>
-              {row.map(cell => Cell(cell.content, cell.key))}
-            </tr>)}
-        </tbody>
-      </table>
-    </div>
+    <>
+      <div className='d-flex'>
+        <Stack gap={GAP}>
+          <Stack gap={GAP} direction="horizontal">
+            <Stack gap={GAP}>
+              <Stack gap={GAP} direction="horizontal">{cells[0]}{cells[1]}{cells[2]}</Stack>
+              <Stack gap={GAP} direction="horizontal">{cells[9]}{cells[10]}{cells[11]}</Stack>
+              <Stack gap={GAP} direction="horizontal">{cells[18]}{cells[19]}{cells[20]}</Stack>
+            </Stack>
+            <div className="vr" />
+            <Stack gap={GAP}>
+              <Stack gap={GAP} direction="horizontal">{cells[3]}{cells[4]}{cells[5]}</Stack>
+              <Stack gap={GAP} direction="horizontal">{cells[12]}{cells[13]}{cells[14]}</Stack>
+              <Stack gap={GAP} direction="horizontal">{cells[21]}{cells[22]}{cells[23]}</Stack>
+            </Stack>
+            <div className="vr" />
+            <Stack gap={GAP}>
+              <Stack gap={GAP} direction="horizontal">{cells[6]}{cells[7]}{cells[8]}</Stack>
+              <Stack gap={GAP} direction="horizontal">{cells[15]}{cells[16]}{cells[17]}</Stack>
+              <Stack gap={GAP} direction="horizontal">{cells[24]}{cells[25]}{cells[26]}</Stack>
+            </Stack>
+          </Stack>
+          <hr className='m-1' />
+          <Stack gap={GAP} direction="horizontal">
+            <Stack gap={GAP}>
+              <Stack gap={GAP} direction="horizontal">{cells[27]}{cells[28]}{cells[29]}</Stack>
+              <Stack gap={GAP} direction="horizontal">{cells[36]}{cells[37]}{cells[38]}</Stack>
+              <Stack gap={GAP} direction="horizontal">{cells[45]}{cells[46]}{cells[47]}</Stack>
+            </Stack>
+            <div className="vr" />
+            <Stack gap={GAP}>
+              <Stack gap={GAP} direction="horizontal">{cells[30]}{cells[31]}{cells[32]}</Stack>
+              <Stack gap={GAP} direction="horizontal">{cells[39]}{cells[40]}{cells[41]}</Stack>
+              <Stack gap={GAP} direction="horizontal">{cells[48]}{cells[49]}{cells[50]}</Stack>
+            </Stack>
+            <div className="vr" />
+            <Stack gap={GAP}>
+              <Stack gap={GAP} direction="horizontal">{cells[33]}{cells[34]}{cells[35]}</Stack>
+              <Stack gap={GAP} direction="horizontal">{cells[42]}{cells[43]}{cells[44]}</Stack>
+              <Stack gap={GAP} direction="horizontal">{cells[51]}{cells[52]}{cells[53]}</Stack>
+            </Stack>
+          </Stack>
+          <hr className='m-1' />
+          <Stack gap={GAP} direction="horizontal">
+            <Stack gap={GAP}>
+              <Stack gap={GAP} direction="horizontal">{cells[54]}{cells[55]}{cells[56]}</Stack>
+              <Stack gap={GAP} direction="horizontal">{cells[63]}{cells[63]}{cells[65]}</Stack>
+              <Stack gap={GAP} direction="horizontal">{cells[72]}{cells[73]}{cells[74]}</Stack>
+            </Stack>
+            <div className="vr" />
+            <Stack gap={GAP}>
+              <Stack gap={GAP} direction="horizontal">{cells[57]}{cells[58]}{cells[59]}</Stack>
+              <Stack gap={GAP} direction="horizontal">{cells[66]}{cells[67]}{cells[68]}</Stack>
+              <Stack gap={GAP} direction="horizontal">{cells[75]}{cells[76]}{cells[77]}</Stack>
+            </Stack>
+            <div className="vr" />
+            <Stack gap={GAP}>
+              <Stack gap={GAP} direction="horizontal">{cells[60]}{cells[61]}{cells[62]}</Stack>
+              <Stack gap={GAP} direction="horizontal">{cells[69]}{cells[70]}{cells[71]}</Stack>
+              <Stack gap={GAP} direction="horizontal">{cells[78]}{cells[79]}{cells[80]}</Stack>
+            </Stack>
+          </Stack>
+        </Stack>
+      </div>
+    </>
   );
 }
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Sudoku from './Sudoku.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* <App /> */}
     <Sudoku />
   </StrictMode>,
 )
+
+// Set theme to the user's preferred color scheme
+function updateTheme() {
+  const colorMode = window.matchMedia("(prefers-color-scheme: dark)").matches ?
+    "dark" :
+    "light";
+  document.querySelector("html")!.setAttribute("data-bs-theme", colorMode);
+}
+
+// Set theme on load
+updateTheme();
+
+// Update theme when the preferred scheme changes
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', updateTheme);
